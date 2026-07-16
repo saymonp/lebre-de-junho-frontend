cffcb0b877bcd3bd64154415599bf404afc155d7
Saymon <saymon.T@outlook.com>
2026-07-16 02:15:57 -0300

    feat: middleware de admin

---
+++ app/middleware/admin.ts
@@ -0,0 +1,16 @@
+import { useAuthStore } from '@/stores/auth'
+
+export default defineNuxtRouteMiddleware((to, from) => {
+    const authStore = useAuthStore()
+
+    // 1. Se o usuário NÃO estiver autenticado, barra e manda para a Home (ou login)
+    if (!authStore.isAuthenticated) {
+        return navigateTo('/')
+    }
+
+    // 2. Se estiver autenticado, mas NÃO for admin, redireciona para a Home ou página de erro
+    if (authStore.user?.roles.includes('admin')) {
+        // Redireciona para a página inicial ou uma tela de "Não Autorizado"
+        return navigateTo('/')
+    }
+})
